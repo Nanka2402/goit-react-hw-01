7df113228f0d385215acd909c67ea003c828a152
Nanka2402 <nastia.secr@gmail.com>
2024-02-20 21:35:16 +0200

    +

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,13 @@
 import userData from "../userData.json";
 import Profile from "./Profile";
 import friends from "../friends.json";
+import FriendList from "./Frendlist";
 
 const App = () => {
   return (
     <>
       <Profile
+        key={userData.id}
         name={userData.username}
         tag={userData.tag}
         location={userData.location}
